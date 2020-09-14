c3d402153ddbe68d701b872fe0727c70d27162b8
DESKTOP-URRPU5K\黃奎嘉 <kevin771011@yahoo.com.tw>
2020-09-14 12:39:28 +0800

    TEST

--- src/podcast/model/idao/ISubProgramListDAO.java
+++ src/podcast/model/idao/ISubProgramListDAO.java
@@ -9,5 +9,5 @@ public interface ISubProgramListDAO {
 	public List<uploadPodcastBean> selectByMemeberId(int memberID) throws Exception;
 
 
-	List<SubscriptionBean> selectSubcriptionByMemberID(int memberID,int podcasterID) throws Exception;
+	public List<SubscriptionBean> selectSubcriptionByMemberID(int memberID,int podcasterID) throws Exception;
 }
