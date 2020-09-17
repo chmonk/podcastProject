d4fd9d2396fee2f5545cf9997d7b50c2ba551ef0
JOJO0B04\JOJO <anchiao0417@gmail.com>
2020-09-18 06:55:22 +0800

    Update ShoppingCart

--- src/podcast/model/dao/OrderDao.java
+++ src/podcast/model/dao/OrderDao.java
@@ -3,18 +3,70 @@ package podcast.model.dao;
 import java.sql.Connection;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import podcast.model.dao.OrderDao;
 import podcast.model.javabean.OrderBean;
 
-public interface OrderDao {
+@Repository
+public class OrderDao implements IOrderDao {
+	
+	private String memberId = null;
+	@Autowired
+	private SessionFactory factory;
+	int orderNo = 0;
 
-	void insertOrder(OrderBean ob);
+	public OrderDao() {
+	}
 
-	void setConnection(Connection con);
+	@Override
+	public void insertOrder(OrderBean ob) {
+		Session session = factory.getCurrentSession();
+        session.save(ob);
+	}
 
-	OrderBean getOrder(int orderNo);
+	public OrderBean getOrder(int orderNo) {
+		OrderBean ob = null;
+        Session session = factory.getCurrentSession();
+        ob = session.get(OrderBean.class, orderNo);
+        return ob;
+	}
 
-	List<OrderBean> getAllOrders();
+	public String getMemberId() {
+		return memberId;
+	}
 
-	List<OrderBean> getMemberOrders(Integer memberId);
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
 
+	public void setConnection(Connection con) {
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<OrderBean> getAllOrders() {
+		List<OrderBean> list = null;
+		String hql = "FROM OrderBean";
+		Session session = factory.getCurrentSession();
+
+		list = session.createQuery(hql).getResultList();
+		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<OrderBean> getMemberOrders(Integer memberId) {
+		List<OrderBean> list = null;
+        Session session = factory.getCurrentSession();
+        String hql = "FROM OrderBean ob WHERE ob.memberId = :mid";
+        list = session.createQuery(hql)
+        			  .setParameter("mid", memberId)
+        			  .getResultList();
+        return list;
+	}
+	
 }
