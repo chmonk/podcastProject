cdf3f89f64c76579b0535044a2ee26b97a8265c3
LAPTOP-2JJ29M3Q\cloud <cloudling623@gmail.com>
2020-09-17 14:47:27 +0800

    jojoactivity

--- src/podcast/controller/ActivityController.java
+++ src/podcast/controller/ActivityController.java
@@ -66,76 +66,36 @@ public class ActivityController {
 
 	// 處理活動表單
 	@RequestMapping(path = "/addActivityProcess", method = RequestMethod.POST)
-    public String processAction(@RequestParam("file") MultipartFile multipartFile,
-    		@RequestParam("radio") int activityStatus,
-    		HttpServletRequest request,@ModelAttribute("ActivityBean") ActivityBean activity,
-    		BindingResult result, Model m) throws Exception {
-    	
-		//檢查所有欄位,有空白則導回表單
-		if(result.hasErrors()) {
-    		return "Activity/addActivity";
-    	}
-    	
-		//model傳送資料
-    	m.addAttribute("activityName", activity.getActivityName());
-    	m.addAttribute("activityDate", activity.getActivityDate());
-    	m.addAttribute("activityLocation", activity.getActivityLocation());
-    	m.addAttribute("activityContent", activity.getActivityContent());
-    	m.addAttribute("activityPrice", activity.getActivityPrice());
-    	
-    	//設定圖片檔名為活動日期
-    	Date activityDate =activity.getActivityDate();
-    	System.out.println("activityId:" + activityDate);
-    	String fileName =activityDate.toString();
-    	//String fileName = multipartFile.getOriginalFilename();
-    	System.out.println("fileName:" + fileName);
-    	     
-    	//設定圖片存檔路徑
-    	String savePath ="C:\\SpringSource\\springworkspace\\PodcastProject2\\WebContent\\WEB-INF\\resources\\images\\"+fileName+".jpg";
-    	//String savePath ="C:\\DataSource\\workspace\\0906PodcastProject\\WebContent\\WEB-INF\\resources\\images\\"+fileName+".jpg";
-    	//String savePath = request.getSession().getServletContext().getRealPath("/") 
-    	// + "uploadTempDir\\" + fileName;
+	public String processAction(@RequestParam("file") MultipartFile multipartFile,
+			@RequestParam("radio") Integer activityStatus, HttpServletRequest request,
+			@ModelAttribute("ActivityBean") ActivityBean activity, BindingResult result, Model m) throws Exception {
 
-    	//存圖片到指定路徑
-    	File saveFile = new File(savePath);
-    	multipartFile.transferTo(saveFile);
-    	
-    	//設定圖片路徑及活動狀態(以@RequestParam取值的屬性)
-    	activity.setActivityImg(savePath);
-    	activity.setActivityStatus(activityStatus);
-    	
-    	//取得資料庫連線
-    	ServletContext app = request.getServletContext();
-    	WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(app);
-    	
-    	//連線到活動資料表
-    	ActivityDAO aDao = (ActivityDAO)context.getBean("ActivityDAO");
-    	
-    	//輸入表單資料至活動資料表
-    	aDao.insert(activity);
-    	
-    	return "Activity/manageActivities";
-    }
-	
-	
-//	//資料庫的所有活動傳送至首頁
-//	@RequestMapping(path = "/a", method = RequestMethod.GET)
-//	public String showActivities(HttpServletRequest request,Model m) throws Exception {
-//		
-//		ServletContext app = request.getServletContext();
-//    	WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(app);
-//    	
-//    	ActivityDAO aDao = (ActivityDAO)context.getBean("ActivityDAO");
-//    	List<ActivityBean> list = new LinkedList<ActivityBean>();
-//    	
-//    	list = aDao.selectAll();
-//
-//		m.addAttribute("list", list);
-//		//return "../../ActivitiesList";
-//		return "/index";
-//		//return "../index";
-//	}
-	
+		// 檢查所有欄位,有空白則導回表單
+		if (result.hasErrors()) {
+			return "Activity/addActivity";
+		}
+
+		MemberBean memberBean = (MemberBean) m.getAttribute("LoginOK");
+		Integer Id = memberBean.getMemberId();
+		
+		String ActivityImg = processFile(Id,multipartFile,request);
+		
+		activity.setActivityImg(ActivityImg);
+		activity.setActivityStatus(activityStatus);
+
+		// 取得資料庫連線
+		ServletContext app = request.getServletContext();
+		WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(app);
+
+		// 連線到活動資料表
+		ActivityDAO aDao = (ActivityDAO) context.getBean("ActivityDAO");
+
+		// 輸入表單資料至活動資料表
+		aDao.insert(activity);
+
+		return "Activity/manageActivities";
+	}
+
 	public String processFile(Integer id,MultipartFile multipartFile,HttpServletRequest request) throws Exception, IOException {
 		// 取得原檔案名字
 		String filename = multipartFile.getOriginalFilename();
