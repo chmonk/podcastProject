c305773060190d8578419f85b8d5907394f1b151
chmonk <l312940@gmail.com>
2020-09-17 15:18:13 +0800

    1.新增controller(/testbar1)轉到測試用的playerbar頁面 2.controller(/gettheplayersong)回傳responentity 範例 3. controller(/postjson) 取得單首歌曲資訊  預計加入player.js mediadata 4. controller(/fuzzy) 導向模糊搜訊測試葉面

--- src/podcast/controller/JsonCreaterController.java
+++ src/podcast/controller/JsonCreaterController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import podcast.model.dao.MemberDAO;
 import podcast.model.dao.UploadPodcastDAO;
 import podcast.model.javabean.CategoryBean;
 import podcast.model.javabean.HouseBean;
@@ -27,6 +28,9 @@ public class JsonCreaterController {
 
 	@Autowired
 	UploadPodcastDAO updao;
+	
+	@Autowired
+	MemberDAO mdao;
 
 	@RequestMapping(path = "/houseBeanJson", method = RequestMethod.GET)
 	public void processAction(Model m) {
@@ -78,14 +82,15 @@ public class JsonCreaterController {
 
 		m.addAttribute("player", a);
 
-		return "playerbar";
+		return "playerBar/playerbar_test";
 	}
 
+	//
 	@GetMapping(value = "/gettheplayersong")
-	public ResponseEntity<List<uploadPodcastBean>> sssString(Model m, @RequestParam("name") Integer mid) {
-
-		System.out.println(mid + "1");
-		System.out.println("aheredscsds");
+	public ResponseEntity<List<uploadPodcastBean>> sssString(Model m,
+			@RequestParam(value = "name", required = false) Integer mid) {
+		// gettheplayersong?name=17
+		System.out.println(mid);
 
 		List<uploadPodcastBean> mlist = updao.queryProgramByMemberID(mid);
 
@@ -94,5 +99,45 @@ public class JsonCreaterController {
 
 		return result;
 	}
+
+	
+	
+	// 取得單首音樂資訊   準備加入播放清單
+	@GetMapping(value = "/postjson")
+	public @ResponseBody Map<String, String> getsinglesong(Model m, @RequestParam("id") Integer podcastid) throws Exception {
+
+		uploadPodcastBean ubean = updao.select(podcastid);
+
+		Map<String, String> songinfo = new HashMap<String, String>();
+		
+		//取得播客member id代號
+		Integer publisherId= ubean.getMemberId();
+		
+		//取得播客暱稱
+		String publishername= mdao.selectPodcaster(publisherId).getNickname();
+		
+//		author: "Freedom1111 Trail Studio",
+//		authorUrl: "https://www.youtube.com/channel/UCx6kpgiQkDkN1UnK5GaATGw",
+//		fileName: "Swing Theory",
+//		fileUrl: "https://s3-ap-northeast-1.amazonaws.com/dazedbear-assets/custom-audio-player/Swing_Theory.mp3",
+//		thumb:"https://s3-ap-northeast-1.amazonaws.com/dazedbear-assets/custom-audio-player/15367448967_0551dce9c1_q.jpg"
+		
+		System.out.println("i am in json");
+		songinfo.put("author", publishername);
+		songinfo.put("authorUrl", "value");
+		songinfo.put("fileName", ubean.getTitle());
+		songinfo.put("fileUrl", ubean.getAudioPath());
+		songinfo.put("thumb", ubean.getAudioimg());
+
+		return songinfo;
+	}
+	
+	
+	//test fuzzyselecttest
+	@GetMapping("fuzzy")
+	public String leadToFuzzy() {
+		return "fuzzySelectTest";
+	}
+	
 
 }
