6f6a716de5fd72a40be089a99827f19004bd858c
chemmonk <l312940@gmail.com>
2020-09-20 23:43:31 +0800

    MyFavProgramDAO 新增兩方法 1.deleteByPodcastID  當節目被刪除時呼叫 2.checkByMemberidAndPodcastID  確認使用者與節目是否有紀錄

--- src/podcast/model/dao/MyFavProgramDAO.java
+++ src/podcast/model/dao/MyFavProgramDAO.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import podcast.model.idao.IMyFavProgramDAO;
+import podcast.model.javabean.LikeRecordBean;
 import podcast.model.javabean.MyFavProgramBean;
 
 @Repository("MyFavProgramDAO")
@@ -68,6 +70,48 @@ public class MyFavProgramDAO implements IMyFavProgramDAO {
 		query.setParameter("podcastId", podcastId);
 
 		return query.list();
+	}
+	
+	public boolean deteleByPodcastId(Integer podcastId) {
+
+		Session session = sessionFactory.getCurrentSession();
+		
+		String nativesqlstr="delete from myFavProgram where podcastId=?";
+		
+		session.createNativeQuery(nativesqlstr).setParameter(1, podcastId).executeUpdate();
+		
+		//檢查是否刪除
+		String nativesqlstr1="select * from myFavProgram where podcastId=?";
+		
+		NativeQuery<MyFavProgramBean> query = session.createNativeQuery(nativesqlstr1,MyFavProgramBean.class).setParameter(1, podcastId);
+		
+		List<MyFavProgramBean> result = query.getResultList();
+		
+		if(result.isEmpty()) {
+			//刪除乾淨
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	//確認有加入播放清單
+	public boolean checkByMemberidAndPodcastID(Integer memberId, Integer podcastId) {
+
+		Session session = sessionFactory.getCurrentSession();
+
+		String sqlstr = "select * from myFavProgram where memberId=? and podcastId=?";
+
+		NativeQuery query = session.createNativeQuery(sqlstr).setParameter(1, memberId).setParameter(2, podcastId);
+
+		List rs = query.getResultList();
+
+		// 如果為空 表示沒訂閱
+		if (rs.isEmpty()) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 }
