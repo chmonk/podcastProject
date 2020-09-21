cdd37feada3881ed2bfde813b23041c65bf66719
chemmonk <l312940@gmail.com>
2020-09-21 18:55:51 +0800

    jsoncontroller getsinglesong同時新增likerecord  瀏覽紀錄  節目點擊數

--- src/podcast/controller/JsonCreaterController.java
+++ src/podcast/controller/JsonCreaterController.java
@@ -26,6 +26,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import podcast.model.dao.HistoryDao;
+import podcast.model.dao.LikeRecordDAO;
 import podcast.model.dao.MemberDAO;
 import podcast.model.dao.MyFavProgramDAO;
 import podcast.model.dao.SubscriptionDAO;
@@ -33,11 +34,12 @@ import podcast.model.dao.UploadPodcastDAO;
 import podcast.model.javabean.CategoryBean;
 import podcast.model.javabean.HistoryBean;
 import podcast.model.javabean.HouseBean;
+import podcast.model.javabean.LikeRecordBean;
 import podcast.model.javabean.MyFavProgramBean;
 import podcast.model.javabean.uploadPodcastBean;
 
 @Controller
-@SessionAttributes({"userid"})
+@SessionAttributes({ "userid" })
 public class JsonCreaterController {
 
 	@Autowired
@@ -45,14 +47,17 @@ public class JsonCreaterController {
 
 	@Autowired
 	MemberDAO mdao;
-	
+
 	@Autowired
 	MyFavProgramDAO myfdao;
-	
+
 	@Autowired
 	SubscriptionDAO sdao;
 
-	//test json用
+	@Autowired
+	LikeRecordDAO ldao;
+
+	// test json用
 	@RequestMapping(path = "/houseBeanJson", method = RequestMethod.GET)
 	public void processAction(Model m) {
 		HouseBean hBean = new HouseBean();
@@ -66,13 +71,12 @@ public class JsonCreaterController {
 		m.addAttribute("categoryBean", cbean);
 	}
 
-	//傳入節目for js mediadata模板
+	// 傳入節目for js mediadata模板
 	@GetMapping(path = "/song")
 	public @ResponseBody Map<String, String> json(Model m) {
 
 		System.out.println("aaa");
 		Map<String, String> json = new HashMap<String, String>();
-
 
 		json.put("author", "米津玄師1111");
 		json.put("authorUrl", "");
@@ -82,8 +86,8 @@ public class JsonCreaterController {
 
 		return json;
 	}
-	
-	//導向player測試業面  並傳回測試用按鈕生成資料
+
+	// 導向player測試業面 並傳回測試用按鈕生成資料
 	@RequestMapping(value = "/testbar1", method = RequestMethod.GET)
 	public String leadToBarOnlyJSP(Model m) {
 
@@ -98,38 +102,31 @@ public class JsonCreaterController {
 
 		m.addAttribute("player", a);
 		m.addAttribute("userid", 2);
-		
 
 		return "playerBar/playerbar_test";
 	}
 
-	//ajax request 送來member id , 傳回member id 所上船節目list  同時增加瀏覽紀錄  
+	// ajax request 送來member id , 傳回member id 所上船節目list 同時增加瀏覽紀錄
 	@GetMapping(value = "/gettheplayersong")
-	public ResponseEntity<List<uploadPodcastBean>> sssString(Model m,
-			HttpServletRequest request,
+	public ResponseEntity<List<uploadPodcastBean>> sssString(Model m, HttpServletRequest request,
 			@RequestParam(value = "name", required = false) Integer mid) {
-		
-		//測試取得使用者id
+
+		// 測試取得使用者id
 		System.out.println(mid);
 
-		//準備ajax檔案
+		// 準備ajax檔案
 		List<uploadPodcastBean> mlist = updao.queryProgramByMemberID(mid);
 		ResponseEntity<List<uploadPodcastBean>> result = new ResponseEntity<List<uploadPodcastBean>>(mlist,
 				HttpStatus.OK);
-		
 
 		return result;
 	}
 
-	// 取得單首音樂資訊 準備加入播放清單  瀏覽列表 新增典籍次數
+	// 取得單首音樂資訊 準備加入播放清單 瀏覽列表 新增典籍次數
 	@GetMapping(value = "/postjson/{userid}/")
-	public @ResponseBody Map<String, String> getsinglesong(Model m,
-			HttpServletRequest request, 
-			@RequestParam("id") Integer podcastId,
-			@PathVariable("userid") Integer userid)
-			throws Exception {
+	public @ResponseBody Map<String, String> getsinglesong(Model m, HttpServletRequest request,
+			@RequestParam("id") Integer podcastId, @PathVariable("userid") Integer userid) throws Exception {
 
-		
 		System.out.println("stage1");
 		uploadPodcastBean ubean = updao.select(podcastId);
 
@@ -141,58 +138,51 @@ public class JsonCreaterController {
 		// 取得播客暱稱
 		String publishername = mdao.selectPodcaster(publisherId).getNickname();
 
-		//單首節目map資訊
-		songinfo.put("podcastId",ubean.getPodcastId().toString());
+		// 單首節目map資訊
+		songinfo.put("podcastId", ubean.getPodcastId().toString());
 		songinfo.put("author", publishername);
 		songinfo.put("authorUrl", "value");
 		songinfo.put("fileName", ubean.getTitle());
 		songinfo.put("fileUrl", ubean.getAudioPath());
 		songinfo.put("thumb", ubean.getAudioimg());
 		System.out.println("stage2");
-		
-		//新增瀏覽紀錄
-				//取得註冊列表
-				ServletContext app = request.getServletContext() ;
-				WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(app);
-				
-				HistoryDao hdao = (HistoryDao)context.getBean("HistoryDao");
-				
-				HistoryBean hbean= new HistoryBean();
-				
-				hbean.setLastListen(new Date());
-				hbean.setMemberId(userid);
-				hbean.setPodcastId(podcastId);
-				hbean.setPodcastName(ubean.getTitle());
-				hbean.setPublisherId(publisherId);
-				
-				hdao.insert(hbean);
-				
-		 //新增點擊數
-				updao.addClickCount(podcastId);
-				System.out.println("stage3");
-				
-				
-		 //如果myfav未有紀錄就新增
-				
-				if(!myfdao.checkByMemberidAndPodcastID(userid, podcastId)) {
-					MyFavProgramBean myfbean = new MyFavProgramBean();
-					myfbean.setMemberId(userid);
-					myfbean.setPodcastId(podcastId);
-					
-					//如果節目需要收費就確認有無註冊
-					if(updao.select(podcastId).getOpenPayment()==1) {
-						myfbean.setSubPermission(sdao.checkSubsription(userid, publisherId));//有訂閱就給過
-					}else {
-						myfbean.setSubPermission(1);//免費就直接核可權限
-					}
-					
-					myfbean.setPodcastOpen(updao.select(podcastId).getOpenPayment());
-					myfbean.setInPlaylist(1);  //預設加入播放清單
-				}
-				
-				
-				
-				
+
+		// 新增瀏覽紀錄
+		// 取得註冊列表
+		ServletContext app = request.getServletContext();
+		WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(app);
+
+		HistoryDao hdao = (HistoryDao) context.getBean("HistoryDao");
+
+		HistoryBean hbean = new HistoryBean();
+
+		hbean.setLastListen(new Date());
+		hbean.setMemberId(userid);
+		hbean.setPodcastId(podcastId);
+		hbean.setPodcastName(ubean.getTitle());
+		hbean.setPublisherId(publisherId);
+
+		hdao.insert(hbean);
+
+		// 新增點擊數
+		updao.addClickCount(podcastId);
+		System.out.println("stage3");
+
+		// 如果likelist未有紀錄就新增 單純新增like record
+
+		LikeRecordBean likeresult = ldao.checkByMemberidAndPodcastID(userid, podcastId);
+		if (likeresult==null) {
+			LikeRecordBean lbean = new LikeRecordBean();
+			lbean.setMemberId(userid);
+			lbean.setPodcastId(podcastId);
+			lbean.setLikeStatus(0);
+			lbean.setShowInListOrNot(1); // 預計顯示在清單中
+
+			ldao.insert(lbean);
+		}else {
+			likeresult.setShowInListOrNot(1); //重點變為可加入list
+		}
+
 		return songinfo;
 	}
 
