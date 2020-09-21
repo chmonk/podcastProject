7fa1acc41b53f7566befbe0a41c42df6ff98d744
lces91429 <lces91429@gmail.com>
2020-09-21 22:51:40 +0800

    index把熱門PODCASTER放上去(進行中)

---
+++ WebContent/WEB-INF/pages/popularPodcastsTry.jsp
@@ -0,0 +1,110 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<!-- Styles -->
+
+<!-- Bootstrap CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Animate CSS -->
+<link href="css/animate.min.css" rel="stylesheet">
+<!-- Basic stylesheet -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+<!-- Font awesome CSS -->
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="css/style.css" rel="stylesheet">
+<link href="css/style-color.css" rel="stylesheet">
+<!-- player bar css -->
+<link href="css/playerbar.css" rel="stylesheet">
+<!-- player bar icon -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="img/logo/favicon.ico">
+
+	<!-- Javascript files -->
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- WayPoints JS -->
+	<script src="js/waypoints.min.js"></script>
+	<!-- Include js plugin -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- One Page Nav -->
+	<script src="js/jquery.nav.js"></script>
+	<!-- Respond JS for IE8 -->
+	<script src="js/respond.min.js"></script>
+	<!-- HTML5 Support for IE -->
+	<script src="js/html5shiv.js"></script>
+	<!-- Custom JS -->
+	<script src="js/custom.js"></script>
+	<!-- 	plaer bar function js -->
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script type="text/javascript" src="js/player.js"></script>
+
+</head>
+<body >
+	<!-- portfolio -->
+	<div class="portfolio pad" id="portfolio">
+		<div>
+			<!-- default heading -->
+			<div class="default-heading">
+				<!-- heading -->
+				<h2>人氣播客</h2>
+			</div>
+		</div>
+		<!-- portfolio -->
+		
+		<div class="portfolio-content" id="portfolioOwl">
+
+			
+		</div>
+		<div class="text-center">
+			<a href="#" class="btn btn-lg btn-theme">View more</a>
+		</div>
+		<!-- portfolio end -->
+	</div>
+	<!-- portfolio end -->
+	
+<script>
+	window.onload=function(){
+		var content=document.getElementById("portfolioOwl");
+		var xhr=new XMLHttpRequest();
+		xhr.open("GET","PopularPodcaster",true);
+		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		xhr.send();
+		xhr.onreadystatechange=function(){
+			if(xhr.readyState===4 && xhr.status===200){
+				var type=xhr.getResponseHeader("Content-Type");
+				var podcasters=JSON.parse(xhr.responseText);
+
+				console.log("podcasters:"+podcasters[3].nickname);
+				var inside="<div class='item'>";
+				for(var i=0;i<podcasters.length;i++){ 
+					inside+="<img class='img-responsive' src='img/portfolio/1.jpg' alt=''>"
+					+"<div class='p-transparent'></div>"
+					+"<div class='p-hover'>"
+					+"<h3>"+podcasters[i].nickname+"</h3>"
+					+"<hr>"
+					+"<p>"+podcasters[i].info+"</p>"
+					+"<a href='#'><i class='fa fa-share'></i></a>"
+					+"</div>"
+					+"</div>";
+				}
+						
+				content.innerHTML=inside;
+			}
+		}
+	}
+</script>
+
+</body>
+
+</html>
--- src/podcast/model/dao/BackStageDAO.java
+++ src/podcast/model/dao/BackStageDAO.java
@@ -1,6 +1,5 @@
 package podcast.model.dao;
 
-
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +22,10 @@ import podcast.model.javabean.SubscriptionBean;
 import podcast.model.javabean.uploadPodcastBean;
 
 @Repository("BackStageDAO")
-public class BackStageDAO implements IBackStageDAO{
-	
-	@Autowired@Qualifier("sessionFactory")
+public class BackStageDAO implements IBackStageDAO {
+
+	@Autowired
+	@Qualifier("sessionFactory")
 	private SessionFactory sessionFactory;
 	@Autowired
 	private MemberDAO mDao;
@@ -43,117 +43,117 @@ public class BackStageDAO implements IBackStageDAO{
 	private HistoryDao hDao;
 	@Autowired
 	private UploadPodcastDAO uDao;
-	
 
 	public BackStageDAO() {
-	
-	}
-	
-	public BackStageDAO(SessionFactory sessionFactory) {
-		this.sessionFactory=sessionFactory;
+
 	}
 
-	//Member FUnction==============================================================================
-	
+	public BackStageDAO(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	// Member
+	// FUnction==============================================================================
+
 	public MemberBean selectMember(Integer memberId) {
-		Session session=sessionFactory.getCurrentSession();
+		Session session = sessionFactory.getCurrentSession();
 		return session.get(MemberBean.class, memberId);
 	}
-	
+
 	public MemberBean selectMemberByAccount(String account) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from MemberBean where account=:account";
-		Query<MemberBean> query=session.createQuery(hqlstr,MemberBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from MemberBean where account=:account";
+		Query<MemberBean> query = session.createQuery(hqlstr, MemberBean.class);
 		query.setParameter("account", account);
 		List<MemberBean> m = query.getResultList();
-		if(m.isEmpty()) {
+		if (m.isEmpty()) {
 			return null;
 		}
-		
+
 		return m.get(0);
 	}
 
 	public boolean deleteMember(Integer memberId) {
-		Session session=sessionFactory.getCurrentSession();
-		MemberBean mBean=session.get(MemberBean.class, memberId);
-		if(mBean!=null) {
+		Session session = sessionFactory.getCurrentSession();
+		MemberBean mBean = session.get(MemberBean.class, memberId);
+		if (mBean != null) {
 			session.delete(mBean);
 			return true;
 		}
 		return false;
 	}
 
-	//TicketOrderFunction==========================================================================
-	
+	// TicketOrderFunction==========================================================================
+
 	public OrderTicketBean selectOrderById(Integer orderId) {
-		Session session=sessionFactory.getCurrentSession();
-		OrderTicketBean oBean=session.get(OrderTicketBean.class, orderId);
+		Session session = sessionFactory.getCurrentSession();
+		OrderTicketBean oBean = session.get(OrderTicketBean.class, orderId);
 		return oBean;
 	}
 
 	public List<OrderTicketBean> selectOrderByMember(Integer memberId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from OrderTicketBean where memberId=:memberId";
-		Query<OrderTicketBean> query=session.createQuery(hqlstr,OrderTicketBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from OrderTicketBean where memberId=:memberId";
+		Query<OrderTicketBean> query = session.createQuery(hqlstr, OrderTicketBean.class);
 		query.setParameter("memberId", memberId);
 		return query.list();
 	}
 
 	public List<OrderTicketBean> selectOrderByActivity(Integer activityId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from OrderTicketBean where activityId=:activityId";
-		Query<OrderTicketBean> query=session.createQuery(hqlstr,OrderTicketBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from OrderTicketBean where activityId=:activityId";
+		Query<OrderTicketBean> query = session.createQuery(hqlstr, OrderTicketBean.class);
 		query.setParameter("activityId", activityId);
 		return query.list();
 	}
-	
-	//ProgramCommentFunction======================================================================
+
+	// ProgramCommentFunction======================================================================
 
 	public ProgramCommentBean selectCommentById(Integer commentId) {
-		Session session=sessionFactory.getCurrentSession();
+		Session session = sessionFactory.getCurrentSession();
 		return session.get(ProgramCommentBean.class, commentId);
 	}
 
-	public List<ProgramCommentBean> selectCommentByMember(Integer memberId,Integer podcasterId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from ProgramCommentBean where memberId=:memberId and podcasterId=:podcasterId";
-		Query<ProgramCommentBean> query=session.createQuery(hqlstr,ProgramCommentBean.class);
+	public List<ProgramCommentBean> selectCommentByMember(Integer memberId, Integer podcasterId) {
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from ProgramCommentBean where memberId=:memberId and podcasterId=:podcasterId";
+		Query<ProgramCommentBean> query = session.createQuery(hqlstr, ProgramCommentBean.class);
 		query.setParameter("memberId", memberId);
 		query.setParameter("podcasterId", podcasterId);
 		return query.list();
 	}
 
 	public List<ProgramCommentBean> selectCommentByPodcaster(Integer podcasterId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from ProgramCommentBean where podcasterId=:podcasterId";
-		Query<ProgramCommentBean> query=session.createQuery(hqlstr,ProgramCommentBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from ProgramCommentBean where podcasterId=:podcasterId";
+		Query<ProgramCommentBean> query = session.createQuery(hqlstr, ProgramCommentBean.class);
 		query.setParameter("podcasterId", podcasterId);
 		return query.list();
 	}// 提醒要改，現有的Bean還是以每集podcast在寫的
 
 	public boolean deleteComment(Integer commentId) {
-		Session session=sessionFactory.getCurrentSession();
-		ProgramCommentBean pBean= session.get(ProgramCommentBean.class, commentId);
-		if(pBean!=null) {
+		Session session = sessionFactory.getCurrentSession();
+		ProgramCommentBean pBean = session.get(ProgramCommentBean.class, commentId);
+		if (pBean != null) {
 			session.delete(pBean);
 			return true;
 		}
 		return false;
 	}
 
-	public void deleteCommentByMember(Integer memberId,Integer podcasterId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from ProgramCommentBean where memberId=:memberId and podcasterId=:podcasterId";
-		Query<ProgramCommentBean> query=session.createQuery(hqlstr,ProgramCommentBean.class);
+	public void deleteCommentByMember(Integer memberId, Integer podcasterId) {
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from ProgramCommentBean where memberId=:memberId and podcasterId=:podcasterId";
+		Query<ProgramCommentBean> query = session.createQuery(hqlstr, ProgramCommentBean.class);
 		query.setParameter("memberId", memberId);
 		query.setParameter("podcasterId", podcasterId);
-		List<ProgramCommentBean> list=query.list();
-		for(ProgramCommentBean pBean:list) {
+		List<ProgramCommentBean> list = query.list();
+		for (ProgramCommentBean pBean : list) {
 			session.delete(pBean);
 		}
 	}
-	
-	//CategoryFunction============================================================
+
+	// CategoryFunction============================================================
 
 	public boolean setNewCategory(String categoryName) throws Exception {
 //		CategoryDAO cDao=new CategoryDAO();
@@ -162,45 +162,47 @@ public class BackStageDAO implements IBackStageDAO{
 //		Query<CategoryBean> query = session.createQuery(hqlstr,CategoryBean.class);
 //		query.setParameter("categoryName", categoryName);
 //		if(query.list().size()==0) {
-			CategoryBean cBean=new CategoryBean();
-			cBean.setCategoryName(categoryName);
-			cDao.insert(cBean);
-			return true;
+		CategoryBean cBean = new CategoryBean();
+		cBean.setCategoryName(categoryName);
+		cDao.insert(cBean);
+		return true;
 //		}
 //		return false;
-		
+
 	}
 
-	public boolean updateCategory(Integer categoryId,CategoryBean cBean) throws Exception {
-		//CategoryDAO cDao=new CategoryDAO();
-		CategoryBean categorybean=cDao.select(categoryId);
-		if(categorybean!=null) {
+	public boolean updateCategory(Integer categoryId, CategoryBean cBean) throws Exception {
+		// CategoryDAO cDao=new CategoryDAO();
+		CategoryBean categorybean = cDao.select(categoryId);
+		if (categorybean != null) {
 			cDao.update(categoryId, cBean);
 			return true;
 		}
 		return false;
 	}
 
-	//Subscription Function==========================================================================
-	
+	// Subscription
+	// Function==========================================================================
+
 	public List<SubscriptionBean> selectSubscriptionByMember(Integer memberId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from SubscriptionBean where memberId=:memberId";
-		Query<SubscriptionBean> query=session.createQuery(hqlstr,SubscriptionBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from SubscriptionBean where memberId=:memberId";
+		Query<SubscriptionBean> query = session.createQuery(hqlstr, SubscriptionBean.class);
 		query.setParameter("memberId", memberId);
 		return query.list();
-	}	
+	}
 
 	public List<SubscriptionBean> selectSubscriptionByPodcaster(Integer podcasterId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from SubscriptionBean where podcasterId=:podcasterId";
-		Query<SubscriptionBean> query=session.createQuery(hqlstr,SubscriptionBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from SubscriptionBean where podcasterId=:podcasterId";
+		Query<SubscriptionBean> query = session.createQuery(hqlstr, SubscriptionBean.class);
 		query.setParameter("podcasterId", podcasterId);
 		return query.list();
 	}
 
-	//Activity Function================================================================================
-	
+	// Activity
+	// Function================================================================================
+
 //	public boolean addNewActivity(ActivityBean aBean) throws Exception {
 //		//ActivityDAO aDao=new ActivityDAO();
 //		ActivityBean activityBean = aDao.select(aBean.getActivityId());
@@ -212,33 +214,34 @@ public class BackStageDAO implements IBackStageDAO{
 //	}
 
 	public ActivityBean selectActivity(Integer activityId) throws Exception {
-		//ActivityDAO aDao=new ActivityDAO();
+		// ActivityDAO aDao=new ActivityDAO();
 		return aDao.select(activityId);
 	}
 
 	public List<ActivityBean> selectActivityByPodcaster(Integer podcasterId) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from ActivityBean where podcasterId=:podcasterId";
-		Query<ActivityBean> query=session.createQuery(hqlstr,ActivityBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from ActivityBean where podcasterId=:podcasterId";
+		Query<ActivityBean> query = session.createQuery(hqlstr, ActivityBean.class);
 		query.setParameter("podcasterId", podcasterId);
 		return query.list();
 	}
 
 	public List<ActivityBean> selectActivitybyDate(Date activityDate) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from ActivityBean where activityDate>:activityDate";
-		Query<ActivityBean> query=session.createQuery(hqlstr,ActivityBean.class);
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from ActivityBean where activityDate>:activityDate";
+		Query<ActivityBean> query = session.createQuery(hqlstr, ActivityBean.class);
 		query.setParameter("activityDate", activityDate);
 		return query.list();
 	}
 
 	public boolean deleteActivity(Integer activityId) throws Exception {
-		//ActivityDAO aDao=new ActivityDAO();
+		// ActivityDAO aDao=new ActivityDAO();
 		return aDao.delete(activityId);
 	}
 
-	//BrowsingHistory Function====================================================================
-	
+	// BrowsingHistory
+	// Function====================================================================
+
 	public HistoryBean selectHistoryById(Integer historyId) {
 		return hDao.select(historyId);
 	}
@@ -250,92 +253,95 @@ public class BackStageDAO implements IBackStageDAO{
 	public List<HistoryBean> selectHistoryByPodcaster(Integer podcasterId) {
 		return hDao.selectByPublisher(podcasterId);
 	}
-	
-	public List<HistoryBean> selectHistoryByLastListen(Date setPoInteger){
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from HistoryBean where lastListen>:setPoInteger";
-		Query<HistoryBean> query=session.createQuery(hqlstr,HistoryBean.class);
+
+	public List<HistoryBean> selectHistoryByLastListen(Date setPoInteger) {
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from HistoryBean where lastListen>:setPoInteger";
+		Query<HistoryBean> query = session.createQuery(hqlstr, HistoryBean.class);
 		query.setParameter("setPoInteger", setPoInteger);
 		return query.list();
 	}
-	
-	public boolean deleteHistoryByDate(Date setPoInteger){
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from HistoryBean where lastListen <:setPoInteger";
-		Query<HistoryBean> query=session.createQuery(hqlstr,HistoryBean.class);
+
+	public boolean deleteHistoryByDate(Date setPoInteger) {
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from HistoryBean where lastListen <:setPoInteger";
+		Query<HistoryBean> query = session.createQuery(hqlstr, HistoryBean.class);
 		query.setParameter("setPoInteger", setPoInteger);
-		List<HistoryBean> list=query.list();
-		for(HistoryBean hBean:list) {
+		List<HistoryBean> list = query.list();
+		for (HistoryBean hBean : list) {
 			session.delete(hBean);
 		}
 		return true;
 	}
-	
-	//income Function===================================================================================
+
+	// income
+	// Function===================================================================================
 
 	@Override
-	public Integer SubscriptionIncome(Date startDate,Date endDate) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from SubscriptionBean where subdateStart between :startDate and :endDate";
-		Query<SubscriptionBean> query=session.createQuery(hqlstr,SubscriptionBean.class);
+	public Integer SubscriptionIncome(Date startDate, Date endDate) {
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from SubscriptionBean where subdateStart between :startDate and :endDate";
+		Query<SubscriptionBean> query = session.createQuery(hqlstr, SubscriptionBean.class);
 		query.setParameter("startDate", startDate);
 		query.setParameter("endDate", endDate);
-		List<SubscriptionBean> list=query.list();
-		
-		Integer income=0;
-		for(SubscriptionBean sBean:list ) {
-			income+=sBean.getMonthlyPayment();
+		List<SubscriptionBean> list = query.list();
+
+		Integer income = 0;
+		for (SubscriptionBean sBean : list) {
+			income += sBean.getMonthlyPayment();
 		}
 		return income;
 	}
 
 	@Override
-	public Integer TicketIncome(Date startDate,Date endDate) {
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from OrderTicketBean where orderDate between :startDate and :endDate";
-		Query<OrderTicketBean> query=session.createQuery(hqlstr,OrderTicketBean.class);
+	public Integer TicketIncome(Date startDate, Date endDate) {
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from OrderTicketBean where orderDate between :startDate and :endDate";
+		Query<OrderTicketBean> query = session.createQuery(hqlstr, OrderTicketBean.class);
 		query.setParameter("startDate", startDate);
 		query.setParameter("endDate", endDate);
-		List<OrderTicketBean> list=query.list();
-		
-		Integer income=0;
-		for(OrderTicketBean oBean:list ) {
-			income+=oBean.getOrderPrice();
+		List<OrderTicketBean> list = query.list();
+
+		Integer income = 0;
+		for (OrderTicketBean oBean : list) {
+			income += oBean.getOrderPrice();
 		}
-		System.out.println("income:"+income);
+		System.out.println("income:" + income);
 		return income;
 	}
-	
-	//UploadPodcast Function===================================================================
-	
+
+	// UploadPodcast
+	// Function===================================================================
+
 	@Override
-	public List<uploadPodcastBean> selectPodcastByMember(Integer memberId){
-		
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from uploadPodcastBean where memberId=:memberId";
-		Query<uploadPodcastBean> query = session.createQuery(hqlstr,uploadPodcastBean.class);
+	public List<uploadPodcastBean> selectPodcastByMember(Integer memberId) {
+
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from uploadPodcastBean where memberId=:memberId";
+		Query<uploadPodcastBean> query = session.createQuery(hqlstr, uploadPodcastBean.class);
 		query.setParameter("memberId", memberId);
-		List<uploadPodcastBean> list=query.list();
-		
+		List<uploadPodcastBean> list = query.list();
+
 		return list;
-		
+
 	}
-	
+
 	@Override
 	public boolean deletePodcast(Integer podcastId) throws Exception {
-		
-		return uDao.delete(podcastId);	
+
+		return uDao.delete(podcastId);
 	}
-	
+
 	@Override
-	public List<uploadPodcastBean> topPodcast(Date uploadTime){
-		
-		Session session=sessionFactory.getCurrentSession();
-		String hqlstr="from uploadPodcastBean where uploadTime>:uploadTime ORDER BY clickAmount DESC ";
-		Query<uploadPodcastBean> query = session.createQuery(hqlstr,uploadPodcastBean.class);
+	public List<uploadPodcastBean> topPodcast(Date uploadTime) {
+
+		Session session = sessionFactory.getCurrentSession();
+		String hqlstr = "from uploadPodcastBean where uploadTime>:uploadTime ORDER BY clickAmount DESC ";
+		Query<uploadPodcastBean> query = session.createQuery(hqlstr, uploadPodcastBean.class);
 		query.setParameter("uploadTime", uploadTime);
-		List<uploadPodcastBean> list=query.list();
-		
+		List<uploadPodcastBean> list = query.list();
+
 		return list;
 	}
-} 
+
+}
