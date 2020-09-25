68281cdb7fcc73b680c5f45a3337808a1e547429
chmonk <l312940@gmail.com>
2020-09-25 11:50:45 +0800

    修正  節目修改無法顯示權限付費頻道問題

--- src/podcast/controller/PodcastController.java
+++ src/podcast/controller/PodcastController.java
@@ -44,7 +44,7 @@ public class PodcastController {
     	UploadPodcastDAO upDao = (UploadPodcastDAO)context.getBean("UploadPodcastDAO");
     	//Integer memberId=(Integer) request.getAttribute("memberId");
 //    	Integer memberId=20;
-    	List<uploadPodcastBean> upList=upDao.selectAllFromMember(memberId);
+    	List<uploadPodcastBean> upList=upDao.selectAllFromMemberAll(memberId);
     	m.addAttribute("upList",upList);
     	request.setAttribute("upList", upList);
 		
@@ -108,7 +108,7 @@ public class PodcastController {
 	    	MemberBean memberBean = (MemberBean) m.getAttribute("LoginOK");
 			Integer memberId = memberBean.getMemberId();
 			
-	    	List<uploadPodcastBean> upList=upDao.selectAllFromMember(memberId);
+	    	List<uploadPodcastBean> upList=upDao.selectAllFromMemberAll(memberId);
 	    	m.addAttribute("upList",upList);
 	    	request.setAttribute("upList", upList);
 			
